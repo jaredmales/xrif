b235bd01afe20715392a21182a4aaa51aedd3ef5
Jared Males <jaredmales@gmail.com>
2019-10-06 11:11:15 -0700

    deleted unused malloc hook stuff

--- tests/xrif_test_init.c
+++ tests/xrif_test_init.c
@@ -84,37 +84,6 @@ matter of this Agreement.
 #include "../src/xrif.h"
 
 
-#ifdef __GLIBC__
-int malloc_hook_active = 0;
-extern void *__libc_malloc(size_t size);
-
-void* my_malloc_hook (size_t size, void *caller)
-{
-  void *result;
-
-  // deactivate hooks for logging
-  malloc_hook_active = 0;
-
-  result = malloc(size);
-
-  fprintf(stderr, "malloc\n");
-
-  // reactivate hooks
-  malloc_hook_active = 1;
-
-  return result;
-}
-
-void* malloc (size_t size)
-{
-  void *caller = __builtin_return_address(0);
-  if (malloc_hook_active)
-    return my_malloc_hook(size, caller);
-  return __libc_malloc(size);
-}
-#endif
-
-
 START_TEST (initialize_handle_noerror)
 {
    //Verify that all fields are initialized to their defaults.
